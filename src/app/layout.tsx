f5419f5750109f72d58fa9367bfc62f0ff097618
jeonseun <jeonseun1234@gmail.com>
2025-08-02 12:36:30 +0900

    chore: components 하위 디렉토리 구조 변경
    - common: 공통 컴포넌트
    - layout: header, footer 등 레이아웃 용 컴포넌트
    - provider: 다크모드 등 특수 기능용 컴포넌트
    - ui: shadcn/ui 용 컴포넌트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css';
-import { ThemeProvider } from '@/components/theme-provider';
+import { ThemeProvider } from '@/components/provider/theme-provider';
 
 export default function RootLayout({
   children,
