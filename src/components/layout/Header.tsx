994f467db281ba4fef940daabd334ad8c3ce1f3f
jeonseun <jeonseun1234@gmail.com>
2025-08-02 12:46:23 +0900

    feat: 다크모드 토글 기능 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link';
+import ThemeToggle from '@/components/common/ThemeToggle';
 
 export function Header() {
   return (
     <header className="flex items-center justify-between p-4">
       <Link href="/">JSU Tech</Link>
 
-      <div></div>
+      <div>
+        <ThemeToggle />
+      </div>
     </header>
   );
 }
